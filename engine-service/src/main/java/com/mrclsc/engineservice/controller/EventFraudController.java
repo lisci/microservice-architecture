d8f77e3d74dbd17e85c6a806de827eb9d852dbca
Marco Lisci <marc.lisci@gmail.com>
2022-08-22 07:48:28 +0200

    Add security EventFraudController

--- engine-service/src/main/java/com/mrclsc/engineservice/controller/EventFraudController.java
+++ engine-service/src/main/java/com/mrclsc/engineservice/controller/EventFraudController.java
@@ -6,6 +6,7 @@ import com.mrclsc.engineservice.service.EventFraudService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@PreAuthorize("isAuthenticated()")  
 @RestController
 @RequestMapping("/engine")
 @AllArgsConstructor
@@ -23,9 +24,14 @@ public class EventFraudController {
     private final EventFraudService eventFraudService;
 
     @GetMapping("/fraud/{typeEvent}/{nameEvent}")
-    public ResponseEntity<List<EventFraud>> getFraud(@PathVariable String typeEvent,
+    public ResponseEntity<?> getFraud(@PathVariable String typeEvent,
                                                      @PathVariable String nameEvent) throws JsonProcessingException {
         return eventFraudService.checkEventFraud(nameEvent, typeEvent);
+    }
+
+    @GetMapping("/fraud")
+    public ResponseEntity<?> getAllFraud() throws JsonProcessingException {
+        return eventFraudService.getAllFraud();
     }
 
 }
