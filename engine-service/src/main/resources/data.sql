4dc04a07a8279d8e364dcb3622d83d0ebe143696
Marco Lisci <marc.lisci@gmail.com>
2023-01-31 21:24:04 +0100

    Update database dump for init purpose

--- engine-service/src/main/resources/data.sql
+++ engine-service/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into user_entity(id, user_username, user_firstname, user_lastname, user_email) values(1,'tony99', 'Antonio','Rossi','antonio.rossi@email.com');
+insert into usr(id, username, firstname, lastname, email, password) values(1,'tony99', 'Antonio','Rossi','antonio.rossi@email.com', '$2a$12$HbQcKnaGnw18oit6k7XCR.gFmWFfvm4bLYh7ul5WQylynYLhD4vda');
+
+insert into roles(id, name) values(1, 'ROLE_USER');
+
+insert into user_roles(user_id, role_id) values(1, 1);
