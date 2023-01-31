0c497ca51126bb2eea861bc18d2a6899e371dc80
Marco Lisci <marc.lisci@gmail.com>
2023-01-31 21:48:40 +0100

    Add swagger interface for EventFraudController

--- engine-service/src/main/java/com/mrclsc/engineservice/controller/EventFraudController.java
+++ engine-service/src/main/java/com/mrclsc/engineservice/controller/EventFraudController.java
@@ -2,11 +2,15 @@ package com.mrclsc.engineservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mrclsc.engineservice.service.EventFraudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.Future;
 
-@PreAuthorize("isAuthenticated()")
+
 @RestController
 @RequestMapping("/engine")
 @AllArgsConstructor
@@ -22,16 +26,30 @@ import java.util.concurrent.Future;
 public class EventFraudController {
 
     private final EventFraudService eventFraudService;
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/fraud/{typeEvent}/{nameEvent}")
+
     @Async
+    @GetMapping("/fraud/{typeEvent}/{nameEvent}")
+    @Operation(summary = "It provides a specific event fraud")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - The fraud was not found")
+    })
+    @Parameters( value = {
+            @Parameter(name = "typeEvent", description = "Event type can be for instance 'IP'"),
+            @Parameter(name = "nameEvent", description = "Event name can be for instance an ip")
+    })
     public Future<ResponseEntity<?>> getFraud(@PathVariable String typeEvent,
                                               @PathVariable String nameEvent) throws JsonProcessingException {
         return eventFraudService.checkEventFraud(nameEvent, typeEvent);
     }
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/fraud")
+
     @Async
+    @GetMapping("/frauds")
+    @Operation(summary = "It provides a event fraud list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - The fraud list was not found")
+    })
     public Future<ResponseEntity<?>> getAllFraud() throws JsonProcessingException {
         return eventFraudService.getAllFraud();
     }
